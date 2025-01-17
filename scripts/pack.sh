712a414d84fe1abfdccd8d399dfb49e477dd2a36
Andrii Rublov <seclerp.dev@gmail.com>
2025-01-17 22:45:09 +0100

    Propagate build number from TeamCity

--- scripts/pack.sh
+++ scripts/pack.sh
@@ -8,10 +8,16 @@ current_dir=$(cd -- "$current_dir" && pwd)
 CONFIGURATION="Release"
 PROJECT="CredentialProvider.Microsoft"
 NUPKG_OUTPUT="./nupkg"
+BUILD_NUMBER=${BUILD_NUMBER:-0}
 
 mkdir -p "$NUPKG_OUTPUT"
 
 echo "> Packing..."
-"$current_dir/dotnet-sdk.cmd" pack --no-build --configuration $CONFIGURATION --output "$NUPKG_OUTPUT" "$PROJECT"
+"$current_dir/dotnet-sdk.cmd" pack \
+  --no-build \
+  --configuration $CONFIGURATION \
+  --output "$NUPKG_OUTPUT" \
+  "$PROJECT" \
+  /p:CredentialProviderBuildNumber="$BUILD_NUMBER"
 
 echo "> Packing completed successfully!"
